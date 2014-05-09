4dd03815fe2a3bf88cad5a613d2f3447f8874652
semiexp <linthya.v@gmail.com>
2014-05-09 17:54:10 +0900

    add problem tester

---
+++ src/testing/problem_tester.cpp
@@ -0,0 +1,68 @@
+
+#include "../main.h"
+#include "../core/nurikabe.h"
+#include "../util/util.h"
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cassert>
+
+std::vector<int> Hs, Ws;
+std::vector<int*> problems;
+
+void load_problem_file()
+{
+	const char problem_file[] = "testing.txt";
+	std::ifstream ifs(problem_file);
+
+	int pCnt; ifs >> pCnt;
+
+	for(int i = 0; i < pCnt; i++) {
+		int H, W;
+
+		ifs >> H >> W;
+
+		Hs.push_back(H);
+		Ws.push_back(W);
+
+		int* prob = new int[H * W];
+		for(int j = 0; j < H * W; j++) {
+			std::string in;
+			ifs >> in;
+
+			prob[j] = parse_int(in);
+		}
+
+		problems.push_back(prob);
+	}
+}
+
+void run_all_problem_test()
+{
+	for(int i = 0; i < Hs.size(); i++) {
+		nk_field fl(Hs[i], Ws[i], problems[i]);
+		
+		nk_solver::assumption(fl, 1);
+
+		assert(fl.status() == nk_field::SOLVED);
+	}
+}
+
+void problem_test()
+{
+	load_problem_file();
+
+	const int TRIAL_COUNT = 50;
+
+	time_t start = clock();
+
+	for(int i = 0; i < TRIAL_COUNT; i++) {
+		run_all_problem_test();
+	}
+
+	time_t end = clock();
+
+	std::cout << "Cost: " << (end - start) / (double)CLOCKS_PER_SEC << std::endl;
+}
